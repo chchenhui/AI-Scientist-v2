59f40a9f6e7bb9b1984ebb27ab0d5bef0d80a12e
chchenhui <chenhui19960614@163.com>
2025-05-22 02:23:59 +0800

    add ideas

--- gen_idea.sh
+++ gen_idea.sh
@@ -1,5 +1,5 @@
 python ai_scientist/perform_ideation_temp_free.py \
- --workshop-file "ai_scientist/ideas/iclr2025_bi_align.md" \
+ --workshop-file "ai_scientist/ideas/iclr2025_data_problems.md" \
  --model google/gemini-2.5-pro-preview \
  --max-num-generations 1 \
  --num-reflections 5
